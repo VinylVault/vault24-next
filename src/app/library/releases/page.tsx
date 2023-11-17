a11fa856f5bfe0efa1c36cf54c61bbe1bcbfe52f
Dex Vinyl <studio@thevinylvaultshow.co.uk>
2023-11-17 13:55:09 +0000

    basic folder structure for pages

---
+++ src/app/library/releases/page.tsx
@@ -0,0 +1,9 @@
+export default function LibraryReleaseList() {
+    return (
+        <div className="flex min-h-screen flex-col items-center p-24 bg-vault-background">
+            <h1 className='text-6xl font-bold font-title text-vault-text'>Hello Vercel!</h1>
+            <h2 className='text-4xl font-bold font-title text-vault-genre'>Welcome To The Future Home Of .....</h2>
+            <h3 className='text-8xl font-bold font-title text-vault-title'>The Vinyl Vault Show Release List</h3>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-vault-background">
+    <div className="flex min-h-screen flex-col items-center p-24 bg-vault-background">
       <h1 className='text-6xl font-bold font-title text-vault-text'>Hello Vercel!</h1>
       <h2 className='text-4xl font-bold font-title text-vault-genre'>Welcome To The Future Home Of .....</h2>
       <h3 className='text-8xl font-bold font-title text-vault-title'>The Vinyl Vault Show</h3>
-    </main>
+    </div>
   )
 }
