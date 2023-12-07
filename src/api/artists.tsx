362cd70a724d8c0a43c2580f970b146bf6053501
Dex Vinyl <studio@thevinylvaultshow.co.uk>
2023-12-07 00:26:36 +0000

    Artist Page & API changes

--- src/api/artists.tsx
+++ src/api/artists.tsx
@@ -2,29 +2,76 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function getAllArtists() {
-    const getArtists = await prisma.libraryArtists.findMany({
-        select: {
-            artistName: true,
-            artistReleaseQuantity: true,
-            artistTrackQuantity: true,
-            artistSlug: true,
-            artistLibrary: true,
+  const getArtists = await prisma.libraryArtists.findMany({
+    select: {
+      artistName: true,
+      artistReleaseQuantity: true,
+      artistTrackQuantity: true,
+      artistSlug: true,
+      artistLibrary: true,
+    },
+    where: {
+      libraryReleases: {
+        some: {
+          libraryShelves: {
+            some: {
+              shelfPrivate: false,
+            },
+          },
         },
+      },
+    },
+    orderBy: {
+      artistName: 'asc',
+    },
+  });
+  prisma.$disconnect();
+  return getArtists;
+}
+
+export async function getOneArtist(artistSlug: string) {
+  // console.log(artistSlug)
+  const getArtistDetails = await prisma.libraryArtists.findUnique({
+    include: {
+      libraryReleases: {
         where: {
-            libraryReleases: {
-                some: {
-                    libraryShelves: {
-                        some: {
-                            shelfPrivate: false,
-                        },
-                    }
-                }
-            }
+          libraryShelves: {
+            some: {
+              shelfPrivate: false,
+            },
+          },
+        },
+        include: {
+          libraryShelves: true,
+          libraryArtists: true,
         },
         orderBy: {
-            artistName: 'asc',
+          releaseAddedToDiscogs: 'desc',
+        },
+      },
+      libraryTracks: {
+        include: {
+          libraryArtists: true,
+          libraryReleases: {
+            where: {
+              libraryShelves: {
+                some: {
+                  shelfPrivate: false,
+                },
+              },
+            },
+          },
+        },
+        orderBy: {
+            trackTitle: 'asc',
         }
-    })
-    prisma.$disconnect()
-    return getArtists
+      },
+    },
+    where: {
+      artistSlug,
+    },
+  });
+  // console.log(getArtistDetails);
+  prisma.$disconnect();
+  return getArtistDetails;
 }
--- src/app/the_vault/artists/[artistSlug]/page.tsx
+++ src/app/the_vault/artists/[artistSlug]/page.tsx
@@ -1,13 +1,62 @@
-"use client"
-import { useParams } from 'next/navigation'
-export default function TheVaultArtistDetails() {
-    const params = useParams()
-    console.log(params)
-    return (
-        <div className="flex min-h-screen flex-col items-center p-24 bg-vault-background">
-            <h1 className='text-6xl font-bold font-title text-vault-text'>Hello Vercel!</h1>
-            <h2 className='text-4xl font-bold font-title text-vault-genre'>Welcome To The Future Home Of .....</h2>
-            <h3 className='text-8xl font-bold font-title text-vault-title'>The Vinyl Vault <span className='text-vault-link capitalize'>{params.artistSlug}</span> Artist Details Page</h3>
-        </div>
-    )
+import { getOneArtist } from '@/api/artists';
+import { Button } from '@/components/Button';
+import { ReleaseGridItems } from '@/components/ReleaseGridItems';
+import Image from 'next/image';
+import Link from 'next/link';
+export default async function TheVaultReleaseDetails({
+  params,
+}: {
+  params: { artistSlug: string };
+}) {
+  // console.log(params)
+
+  const localArtist = await getOneArtist(params.artistSlug);
+
+  return (
+    <div className="flex min-h-screen flex-col p-12 bg-vault-background">
+      <h3 className="text-8xl font-bold font-title text-center pb-4 text-vault-title">
+        Artist Details For: <br />
+        <span className="text-vault-link capitalize">
+          {localArtist?.artistName}
+        </span>
+      </h3>
+      <h4
+        id="releases"
+        className="text-4xl font-bold font-title text-vault-genre p-4"
+      >
+        Releases:
+      </h4>
+      <div className="grid gap-4 w-full lg:grid-cols-5 md:grid-cols-2 grid-cols-1 mr-4">
+        {localArtist?.libraryReleases.map((release) => (
+          <ReleaseGridItems key={release.releaseSlug} {...release} />
+        ))}
+      </div>
+      <h4
+        id="tracks"
+        className="text-4xl font-bold font-title text-vault-genre p-4"
+      >
+        Tracks:
+      </h4>
+      <div>
+        {localArtist?.libraryTracks.map((track) => (
+          <>
+            <p
+              className="text-vault-text mx-2 mb-1 text-lg"
+              key={track.trackSlug}
+            >
+              <strong>Track Title:</strong> {track.trackTitle}
+            </p>
+            {track.libraryArtists.map((artist) => (
+              <p
+                className="text-vault-text mx-2 mb-1 text-lg"
+                key={artist.artistSlug}
+              >
+                <strong>Track Artist:</strong> {artist.artistName}
+              </p>
+            ))}
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
